049f953a21fe8681a36efc8199a116135c9f7c9d
mehdi-mtir <mehdi.mtir@gmail.com>
2022-10-17 17:27:06 +0100

    bookstore app with list only

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../model/book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  listBooks = [
+    new Book(1, "The Slight Edge", "Jeff Olsen", "23.00"),
+    new Book(2, "Atomic Habits", "James Clear", "18.00"),
+    new Book(3, "So Good They Can't ignore you", "Cal Newport", "18.70")
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
